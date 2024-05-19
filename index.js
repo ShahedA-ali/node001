8768a9b5ed5f907632bc0a611d84a565479d1ac2
AliKhalid <ali16kalid@gmail.com>
2024-05-19 15:37:31 +0430

    origin and cors problem

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require('express')
 const app = express()
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8000;
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+const myLogger = function (req, res, next) {
+    console.log('LOGGED')
+    next()
+  }
 
+app.use(myLogger);
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.send({result: 'Hello, World'});
 });
 
 app.listen(port, hostname, () => {
